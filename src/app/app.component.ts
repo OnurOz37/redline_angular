a16c04a20e5e3eae9705524783d6878167cada0c
Oozogul <onur.o@emiles.fr>
2023-05-05 14:07:30 +0200

    Ajout prjt

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+/*
+* D’abord, on importe les éléments dont on va avoir besoin dans notre fichier.
+* A la ligne 1, on importe l’annotation Component depuis le cœur de Angular
+* */
+import { Component } from '@angular/core';
+
+//Définition du composant
+@Component({
+  selector: 'app-root', //donner un nom a son composant
+  templateUrl: './templates/app.header.component.html',
+  styleUrls: ['./css/app.header.component.css', 'css/app.general.component.css']
+})
+export class AppComponent {
+  name = 'Onur'
+  title= "SIUU";
+}
