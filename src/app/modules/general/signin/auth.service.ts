462497dc1333b0ddc9bdc7ab7a13545ee0d19ace
johannweytens <weytensjohann771@gmail.com>
2023-05-31 10:44:39 +0200

    amélioration du login et ajout  méthode de déconnexion

--- src/app/modules/general/signin/auth.service.ts
+++ src/app/modules/general/signin/auth.service.ts
@@ -27,4 +27,8 @@ export class AuthService {
         }
         return null;
     }
+    removeAccessToken(): void {
+        localStorage.removeItem(this.accessTokenKey);
+    }
+
 }
--- src/app/modules/general/signin/signin.service.ts
+++ src/app/modules/general/signin/signin.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import {tap} from "rxjs";
+import {map, tap} from "rxjs";
+import {catchError} from "rxjs/operators";
 
 
 @Injectable({
@@ -11,15 +12,19 @@ export class SigninService {
     constructor(private http: HttpClient, private authService: AuthService) {}
 
     login(username: string, password: string) {
-        return this.http.post("/login", { username, password }).pipe(
-            tap((response: any) => {
-                console.log("hello");
-                console.log(response)
-                const accessToken = response.accessToken;
-                console.log(accessToken);
-                console.log("hello2");
-                this.authService.setAccessToken(accessToken);
-                console.log("hello3");
+        return this.http.post("/login", { username, password }, { observe: 'response' }).pipe(
+            tap(response => {
+                const accessToken = response.headers.get('Authorization');
+                console.log('Response from API:', response);
+                console.log('Access token:', accessToken);
+                if (accessToken) {
+                    const bearerToken = accessToken.replace('Bearer ', '');
+                    console.log('Bearer token:', bearerToken);
+                    this.authService.setAccessToken(bearerToken);
+                } else {
+                    console.error('Bearer token not found in response headers');
+                }
+                return response.body;
             })
         );
     }
