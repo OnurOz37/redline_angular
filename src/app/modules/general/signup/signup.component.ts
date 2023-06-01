388413cb6e8f5f1ba52dfea4099753c8fa3a711c
johannweytens <weytensjohann771@gmail.com>
2023-06-01 11:21:24 +0200

    création d'utilisateur

--- src/app/modules/general/signup/signup.component.ts
+++ src/app/modules/general/signup/signup.component.ts
@@ -1,15 +1,47 @@
 import { Component } from '@angular/core';
+import { UserService } from './signup.service';
 
 @Component({
-  selector: 'app-signup',
-  template: `
-    <p>
-      signup works!
-    </p>
-  `,
-  styles: [
-  ]
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css'],
+    providers: [UserService] // Ajoutez le service comme fournisseur
 })
 export class SignupComponent {
+    username: string;
+    password: string;
+    nom: string;
+    mail: string;
+    motDePasse: string;
+    idRole: any;
 
+    constructor(private userService: UserService) {}
+
+    createUser() {
+        if (!this.nom || !this.mail || !this.motDePasse || !this.idRole) {
+            alert('Veuillez remplir tous les champs obligatoires.');
+            return;
+        }
+
+        const userData = {
+            nom: this.nom,
+            mail: this.mail,
+            motDePasse: this.motDePasse,
+            idRole: this.idRole
+        };
+
+        this.userService.createUser(userData).subscribe(
+            response => {
+                alert('Utilisateur créé avec succès.');
+                this.nom = '';
+                this.mail = '';
+                this.motDePasse = '';
+                this.idRole = null;
+            },
+            error => {
+                console.error("Erreur lors de la création de l'utilisateur:", error);
+                alert("Une erreur s'est produite lors de la création de l'utilisateur. Veuillez réessayer.");
+            }
+        );
+    }
 }
